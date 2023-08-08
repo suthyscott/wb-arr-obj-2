2a540625dc70cb1f4581359e22ee495e4aa83610
suthyscott <sutherlandscott21@gmail.com>
2023-08-08 15:48:57 -0600

    little stuff

--- higher-order.js
+++ higher-order.js
@@ -128,10 +128,7 @@ const orders = [
   Use a higher order method to get all the order totals after adding in the sales tax (given to you as a tax rate, hint: you'll need to do some multiplication). Your answer should be an array of numbers, one total for each order.
 */
 
-// let orderTotals = orders.map(order => order.price * (1 + order.tax))
-let orderTotals = orders.map(ele => {
-  return ele.price += (ele.price * ele.tax)
-}) 
+let orderTotals = orders.map(order => order.price * (1 + order.tax))
 
 console.log(orderTotals)
 
@@ -171,3 +168,5 @@ const purchases = [
 let bobsTotal = purchases.reduce((acc, purchase) => {
   return purchase.owner === 'Bob' ? acc + purchase.price : acc
 }, 0)
+
+console.log(bobsTotal)
